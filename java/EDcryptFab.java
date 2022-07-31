6c7c1b7d841f6b8f990797cc166d26eb8e8d2279
Siddhartha <48620561+TerminatorOTW@users.noreply.github.com>
2022-07-31 12:40:39 +0530

    adding icon to the app

--- java/EDcryptFab.java
+++ java/EDcryptFab.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import edcryptfab.ResourceLoader;
 
 public class EDcryptFab extends Application {
 
@@ -19,6 +20,7 @@ public class EDcryptFab extends Application {
         
         primaryStage.setScene(scene);
         primaryStage.setTitle("EDCryptFab V1.1");
+        primaryStage.getIcons().add(new ResourceLoader().loadImages("app_icon.png"));
         primaryStage.show();
         
     }
